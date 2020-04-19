6ea164a1963a545d608750fa592b22b8ed776047
sarthak jain <mrsarthak001@gmail.com>
2020-04-19 16:11:22 +0530

    Assignment_2

---
+++ Assignment 2/sarthak.py
@@ -0,0 +1,13 @@
+
+import os
+
+for num in range(1,101):
+	if num<=25:
+		os.system("docker run -itd --restart unless-stopped --name adhocnw{} alpine".format(num))
+	elif num<=50:
+		os.system("docker run -itd --restart unless-stopped --name adhocnw{} fedora".format(num))
+	elif num<=75:
+		os.system("docker run -itd --restart unless-stopped --name adhocnw{} centos".format(num))
+	elif num<=100:
+		os.system("docker run -itd --restart unless-stopped --name adhocnw{} java".format(num))
+os.system("docker ps")
